c8227bacbdea5dc1620f2d8c3d1baab2b23d2311
MrLotU <j.koopman@jarict.nl>
2019-02-26 21:53:38 +0100

    [broken] Added some more structs
    Signed-off-by: MrLotU <j.koopman@jarict.nl>

--- Sources/OpenAPI/OpenApiProvider.swift
+++ Sources/OpenAPI/OpenApiProvider.swift
@@ -56,7 +56,7 @@ public struct OpenAPIServerVariable: Content {
     public let description: String?
 }
 
-public protocol RegexableString: LosslessStringConvertible {
+public protocol RegexableString: LosslessStringConvertible, Hashable {
     
     static var regex: String { get }
     
@@ -116,7 +116,51 @@ public struct OpenAPIPathString: RegexableString {
 }
 
 public struct OpenAPIPath: Content {
-    
+    public let summary: String?
+    public let description: String?
+    public let get: OpenAPIOperation?
+    public let put: OpenAPIOperation?
+    public let post: OpenAPIOperation?
+    public let delete: OpenAPIOperation?
+    public let options: OpenAPIOperation?
+    public let head: OpenAPIOperation?
+    public let patch: OpenAPIOperation?
+    public let trace: OpenAPIOperation?
+    public let servers: [OpenAPIServer]?
+    public let parameters: [OpenAPIParameter]?
+}
+
+public struct OpenAPIOperation: Content {
+    public let tags: [String]?
+    public let summary: String?
+    public let description: String?
+    public let externalDocs: OpenAPIExternalDocs?
+    public let operationId: String
+    public let parameters: [OpenAPIParameter]?
+    public let requestBody: OpenAPIRequestBody?
+    public let responses: OpenAPIResponses
+    public let callbacks: [String: OpenAPICallback]?
+    public let deprecated: Bool?
+    public let security: [OpenAPISecurityRequirement]?
+    public let servers: [OpenAPIServer]?
+}
+
+public struct OpenAPIExternalDocs: Content {
+    public let description: String?
+    public let url: URL
+}
+
+public struct OpenAPIParameter: Content {
+    public let name: String
+    public let `in`: OpenAPIParameterLocation
+    public let description: String?
+    public private(set) var required: Bool; #warning(#"TODO: If the parameter location is "path", this property is REQUIRED and its value MUST be true. Otherwise, the property MAY be included and its default value is false."#)
+    public let deprecated: Bool?
+    public let allowEmptyValue: Bool
+}
+
+public enum OpenAPIParameterLocation: String, Content {
+    case query, header, path, cookie
 }
 
 
